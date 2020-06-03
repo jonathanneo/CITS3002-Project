3b6bc9a45f2fe9e4f533c3101921c2c1196f6957
Jonathan <jonathanneo@me.com>
2020-06-04 01:06:22 +0800

    working

--- startstations.sh
+++ startstations.sh
@@ -1,10 +1,50 @@
-python3 station.py TerminalA 4001 4002 4008 4014 4016 &
-python3 station.py JunctionB 4003 4004 4006 4012 4016 4018 &
-python3 station.py BusportC 4005 4006 4004 4014 4020 &
-python3 station.py StationD 4007 4008 4002 4012 &
-python3 station.py TerminalE 4009 4010 4016 &
-python3 station.py JunctionF 4011 4012 4004 4008 4014 &
-python3 station.py BusportG 4013 4014 4002 4006 4012 4016 4018 4020 &
-python3 station.py StationH 4015 4016 4002 4004 4010 4014 &
-python3 station.py TerminalI 4017 4018 4004 4014 &
-python3 station.py JunctionJ 4019 4020 4006 4014 
+python3 station.py TerminalAA 4001 4002 4042 4056 4086 4090 4098 &
+python3 station.py JunctionBA 4003 4004 4008 4010 4012 4068 4074 &
+python3 station.py BusportCA 4005 4006 4012 4042 &
+python3 station.py StationDA 4007 4008 4004 4030 4034 4044 4048 4054 4086 &
+python3 station.py TerminalEA 4009 4010 4004 4014 4018 4026 &
+python3 station.py JunctionFA 4011 4012 4004 4006 4030 4046 4066 4094 &
+python3 station.py BusportGA 4013 4014 4010 4088 4094 4098 4100 &
+python3 station.py StationHA 4015 4016 4050 4054 4072 &
+python3 station.py TerminalIA 4017 4018 4010 4030 4040 &
+python3 station.py JunctionJA 4019 4020 4028 4084 &
+python3 station.py BusportKA 4021 4022 4066 4072 4086 &
+python3 station.py StationLA 4023 4024 4030 4044 4100 &
+python3 station.py TerminalMA 4025 4026 4010 4062 &
+python3 station.py JunctionNA 4027 4028 4020 4074 4078 &
+python3 station.py BusportOA 4029 4030 4008 4012 4018 4024 4074 4076 4088 &
+python3 station.py StationPA 4031 4032 4040 &
+python3 station.py TerminalQA 4033 4034 4008 4084 &
+python3 station.py JunctionRA 4035 4036 4058 4094 &
+python3 station.py BusportSA 4037 4038 4058 4088 &
+python3 station.py StationTA 4039 4040 4018 4032 4096 4100 &
+python3 station.py TerminalUA 4041 4042 4002 4006 4056 4086 4088 4092 4100 &
+python3 station.py JunctionVA 4043 4044 4008 4024 4046 4062 4070 &
+python3 station.py BusportWA 4045 4046 4012 4044 4082 4086 4096 &
+python3 station.py StationXA 4047 4048 4008 4072 4074 4088 &
+python3 station.py TerminalYA 4049 4050 4016 &
+python3 station.py JunctionZA 4051 4052 4086 4098 &
+python3 station.py BusportAB 4053 4054 4008 4016 &
+python3 station.py StationBB 4055 4056 4002 4042 4058 4074 4094 &
+python3 station.py TerminalCB 4057 4058 4036 4038 4056 4080 4082 4094 &
+python3 station.py JunctionDB 4059 4060 4066 4068 4090 &
+python3 station.py BusportEB 4061 4062 4026 4044 4064 4068 &
+python3 station.py StationFB 4063 4064 4062 4098 &
+python3 station.py TerminalGB 4065 4066 4012 4022 4060 4074 4088 &
+python3 station.py JunctionHB 4067 4068 4004 4060 4062 4076 &
+python3 station.py BusportIB 4069 4070 4044 4072 4078 &
+python3 station.py StationJB 4071 4072 4016 4022 4048 4070 4086 4094 &
+python3 station.py TerminalKB 4073 4074 4004 4028 4030 4048 4056 4066 4098 &
+python3 station.py JunctionLB 4075 4076 4030 4068 &
+python3 station.py BusportMB 4077 4078 4028 4070 4082 4096 &
+python3 station.py StationNB 4079 4080 4058 &
+python3 station.py TerminalOB 4081 4082 4046 4058 4078 4090 &
+python3 station.py JunctionPB 4083 4084 4020 4034 &
+python3 station.py BusportQB 4085 4086 4002 4008 4022 4042 4046 4052 4072 4088 4100 &
+python3 station.py StationRB 4087 4088 4014 4030 4038 4042 4048 4066 4086 4096 &
+python3 station.py TerminalSB 4089 4090 4002 4060 4082 &
+python3 station.py JunctionTB 4091 4092 4042 &
+python3 station.py BusportUB 4093 4094 4012 4014 4036 4056 4058 4072 &
+python3 station.py StationVB 4095 4096 4040 4046 4078 4088 &
+python3 station.py TerminalWB 4097 4098 4002 4014 4052 4064 4074 &
+python3 station.py JunctionXB 4099 4100 4014 4024 4040 4042 4086
